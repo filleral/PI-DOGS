64e432768bb40c8c201d3b64e8c002b921729f47
Filleral Mahecha <filleral19@gmail.com>
2023-03-28 21:02:30 -0500

    finish him

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const {PORT} = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('escuchando en http://localhost:3001/'); // eslint-disable-line no-console
+conn.sync({ alter: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log('escuchando en ' + PORT); // eslint-disable-line no-console
   });
 });
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,152 +1,187 @@
-const { Router} = require('express');
-const axios = require('axios');
-const {Dog, Temperament} = require("../db.js")
-const {API, API_SEARCH, API_KEY} = process.env;
-const {formateoDb, formateoApi} = require("../controllers/controllers");
-const { Op } = require('sequelize');
+const { Router } = require("express");
+const axios = require("axios");
+const { Dog, Temperament } = require("../db.js");
+const { API, API_SEARCH, API_KEY } = process.env;
+const { formateoDb, formateoApi } = require("../controllers/controllers");
+const { Op } = require("sequelize");
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.get('/search', async(req, res, next) => {
-  const {name} = req.query;
+router.get("/search", async (req, res, next) => {
+  const { name } = req.query;
   try {
+    const dogApi = (await axios.get(`${API}?api_key=${API_KEY}`)).data;
+    const dogDb = await Dog.findAll({
+      where: { name: { [Op.iLike]: `${name}%` } },
+      include: Temperament,
+    });
 
-    const dogApi = (await axios.get(`${API}?api_key=${API_KEY}`)).data
-    const dogDb = await Dog.findAll({where: {name: {[Op.iLike]:`${name}%`}}, include: Temperament})
+    const validandoDogsDb = await formateoDb(dogDb);
+    const validandoDogsApi = await formateoApi(dogApi);
 
-    const validandoDogsDb = await formateoDb(dogDb)
-    const validandoDogsApi = await formateoApi(dogApi)
+    const allDog = await validandoDogsApi.concat(validandoDogsDb);
 
-    const allDog = await validandoDogsApi.concat(validandoDogsDb)
-
-    if(!name){
-      res.send(allDog)
+    if (!name) {
+      res.send(allDog);
     } else {
+      const dog = await allDog.filter((d) =>
+        d.name.toLowerCase().includes(name.toLowerCase())
+      );
 
-      const dog = await allDog.filter(d => d.name.toLowerCase().includes(name.toLowerCase()))
-        
-      return res
-        .status(200)
-        .send(dog)
+      return res.status(200).send(dog);
     }
-
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-
-
-
-router.get('/dogs', async(req, res, next) => {
+router.get("/dogs", async (req, res, next) => {
   try {
-    const dogApi = (await axios.get(`${API}?api_key=${API_KEY}`)).data
-    const dogDb = await Dog.findAll({include: Temperament});
+    const dogApi = (await axios.get(`${API}?api_key=${API_KEY}`)).data;
+    const dogDb = await Dog.findAll({ include: Temperament });
 
-    const validandoDogsDb = await formateoDb(dogDb)
-    const validandoDogsApi = await formateoApi(dogApi)
+    const validandoDogsDb = await formateoDb(dogDb);
+    const validandoDogsApi = await formateoApi(dogApi);
 
-    const allDog = await validandoDogsApi.concat(validandoDogsDb)
-    
-    res.json(allDog)
+    const allDog = await validandoDogsApi.concat(validandoDogsDb);
 
+    res.json(allDog);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
+// router.post('/dogs', async(req, res) => {
+//   const {name, height_min, height_max, weight_min, weight_max, temperament} = req.body;
+//   if(!name || !height_min || !height_max || !weight_min || !weight_max) {
+//     return res
+//       .status(400)
+//       .send({msg: "Falta enviar datos obligatorios"})
+//   }
+//   try {
+//     const dog = await Dog.create(req.body)
 
+//     let tempDb = await Temperament.findAll({
+//       where: {id : temperament}
+//     })
 
+//     await dog.addTemperament(temperament)
+//     return res
+//       .status(201)
+//       .send({msg: "Perro creado correctamente"})
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
 
-router.post('/dogs', async(req, res) => {
-  const {name, height_min, height_max, weight_min, weight_max, temperament} = req.body;
-  if(!name || !height_min || !height_max || !weight_min || !weight_max) {
-    return res
-      .status(400)
-      .send({msg: "Falta enviar datos obligatorios"})
-  }
+router.post("/dogs", async (req, res, next) => {
+  const {
+    name,
+    height_min,
+    height_max,
+    weight_min,
+    weight_max,
+    life_span_min,
+    life_span_max,
+    image,
+    temperament, // arreglo de nombres de temperamentos
+  } = req.body;
   try {
-    const dog = await Dog.create(req.body)
+    // Crear registro de Dog
+    let dogCreated = await Dog.create({
+      name,
+      height_min,
+      height_max,
+      weight_min,
+      weight_max,
+      life_span_min,
+      life_span_max,
+      image,
+    });
+    await dogCreated.addTemperament(temperament);
 
-    let tempDb = await Temperament.findAll({
-      where: {id : temperament}
-    })
+    // Asociar temperamentos
+    // if (temperament.length) {
+    //   const temperamentPromises = temperament.map(async (tem) => {
+    //     try {
+    //       // Encontrar o crear registro de Temperament
+    //       const [temper, created] = await Temperament.findOrCreate({
+    //         where: { name: tem },
+    //       });
+    //       // Asociar Temperament a Dog
+    //       await dogCreated.addTemperament(temper);
+    //       console.log(`Temperament ${tem} added to Dog ${dogCreated.id}`);
+    //     } catch (err) {
+    //       console.log(`Error adding temperament ${tem} to Dog ${dogCreated.id}`);
+    //     }
+    //   });
+    //   await Promise.all(temperamentPromises);
+    // }
 
-    await dog.addTemperament(temperament)
-
-    return res
-      .status(201)
-      .send({msg: "Perro creado correctamente"})
+    res.send("Dog created successfully!");
   } catch (error) {
-    console.log(error)
+    next(error);
   }
-})
+});
 
-
-
-
-router.get('/temperaments', async(req, res, next)=> {
+router.get("/temperaments", async (req, res, next) => {
   try {
-    const temperamentos = (await axios.get(`${API}?api_key=${API_KEY}`)).data
-    const formateo = temperamentos.map(t => t.temperament)
-    const uniendo = formateo.filter(r => r != null)
-    .join().split(", ").join().split(",")
+    const temperamentos = (await axios.get(`${API}?api_key=${API_KEY}`)).data;
+    const formateo = temperamentos.map((t) => t.temperament);
+    const uniendo = formateo
+      .filter((r) => r != null)
+      .join()
+      .split(", ")
+      .join()
+      .split(",");
 
     let resultado = uniendo.reduce((a, e) => {
-      if(!a.find(d => d == e)) a.push(e)
-      return a
+      if (!a.find((d) => d == e)) a.push(e);
+      return a;
     }, []);
 
-    resultado = resultado.map(t => {return{name: t}})
+    resultado = resultado.map((t) => {
+      return { name: t };
+    });
 
-    const allTemps = await Temperament.findAll()
-    
-    if(allTemps.length === 0) {
-      await Temperament.bulkCreate(resultado)
-    } 
-    const temper = await Temperament.findAll()
-      
-    res.send(temper)
+    const allTemps = await Temperament.findAll();
 
+    if (allTemps.length === 0) {
+      await Temperament.bulkCreate(resultado);
+    }
+    const temper = await Temperament.findAll();
+
+    res.send(temper);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-
-
-
-router.get('/dogs/:idRaza', async(req, res, next) => {
-  const {idRaza} = req.params;
-  if(!idRaza) {
-    return res
-      .status(400)
-      .send({msg: "Falta enviar datos obligatorios"})
+router.get("/dogs/:idRaza", async (req, res, next) => {
+  const { idRaza } = req.params;
+  if (!idRaza) {
+    return res.status(400).send({ msg: "Falta enviar datos obligatorios" });
   }
   try {
-    const dogApi = (await axios.get(`${API}?api_key=${API_KEY}`)).data
-    const dogDb = await Dog.findAll({include: Temperament});
+    const dogApi = (await axios.get(`${API}?api_key=${API_KEY}`)).data;
+    const dogDb = await Dog.findAll({ include: Temperament });
 
-    const validandoDogsDb = await formateoDb(dogDb)
-    const validandoDogsApi = await formateoApi(dogApi)
+    const validandoDogsDb = await formateoDb(dogDb);
+    const validandoDogsApi = await formateoApi(dogApi);
 
-    const allDog = await validandoDogsDb.concat(validandoDogsApi)
+    const allDog = await validandoDogsDb.concat(validandoDogsApi);
 
-    const dog = allDog.filter(d => d.id == idRaza)
+    const dog = allDog.filter((d) => d.id == idRaza);
 
-    return res
-      .status(200)
-      .send(dog)
+    return res.status(200).send(dog);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
-
+});
 
 module.exports = router;
